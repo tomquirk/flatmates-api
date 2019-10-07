34aebcb5ad2fd27d2b09827c1c968a23264f0a73
Tom Quirk <tomquirkacc@gmail.com>
2019-10-07 11:46:49 +1000

    fix search

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 🏠 Python Wrapper for flatmates.com.au
 
 > No "official" API access required - just use a valid Flatmates account!
+
+### Example usage
+
+1. Log in on the website, and get a _sessionId, _flatmates_session_id and csrf token, then:
+
+```python
+api = Flatmates(
+    sessionId="abcd",
+    flatmatesSessionId="abcd",
+    csrfToken="abcd",
+)
+
+# search people
+people = api.search(location="west-end-4101", min_price=300, max_depth=1)
+
+# send message
+person = people[0]
+api.send_message(person.get("id"), "Hey man, come live with me")
+```
--- flatmates_api/flatmates.py
+++ flatmates_api/flatmates.py
@@ -5,7 +5,6 @@ import random
 import logging
 from time import sleep
 import json
-from bs4 import BeautifulSoup
 
 from flatmates_api.client import Client
 
@@ -59,21 +58,92 @@ class Flatmates(object):
 
         return res.status_code == 201
 
-    def search(self, query, max_depth=-1, _listings=[]):
-        res = self.client.session.post(
-            f"{self.client.API_BASE_URL}/search.json",
-            json={"search": query},
-            headers={"Content-Type": "application/json;charset=UTF-8"},
-        )
-        listing_results = res.json()
-        _listings.extend(listing_results["listings"])
+    def _build_query(
+        self,
+        location=None,  # west-end-4101
+        available_from=None,  # 25-10-2019
+        min_age=None,
+        max_age=None,
+        lgtb=False,
+        no_kids=False,
+        no_pets=False,
+        non_smoker=False,
+        min_price=-1,
+        max_price=-1,
+        room_type=None,
+    ):
+        query = [
+            f"available-{available_from}" if available_from else None,
+            "lgtb" if lgtb else None,
+            "no_kids" if no_kids else None,
+            "no_pets" if no_pets else None,
+            "non_smoker" if non_smoker else None,
+            "room_type" if room_type else None,
+            f"min-{min_price}" if min_price else None,
+            f"max-{max_price}" if max_price else None,
+            f"max-{max_age}yrs" if max_age else None,
+            f"max-{min_age}yrs" if min_age else None,
+        ]
+        location = location + "/" if location else ""
+        return f"{location}{'+'.join([i for i in query if i])}"
 
-        next_page = listing_results["nextPage"]
+    def search(
+        self,
+        location=None,
+        available_from=None,
+        min_age=None,
+        max_age=None,
+        lgtb=False,
+        no_kids=False,
+        no_pets=False,
+        non_smoker=False,
+        min_price=None,
+        max_price=None,
+        room_type=None,
+        max_depth=-1,
+        page=1,
+        _listings=[],
+    ):
+        query = self._build_query(
+            location=location,
+            available_from=available_from,
+            min_age=min_age,
+            max_age=max_age,
+            lgtb=lgtb,
+            no_kids=no_kids,
+            no_pets=no_pets,
+            non_smoker=non_smoker,
+            min_price=min_price,
+            max_price=max_price,
+            room_type=room_type,
+        )
+        url = f"{self.client.API_BASE_URL}/people/{query}?page={page}"
+
+        res = self.client.session.get(url)
+        res = res.json()
+        _listings.extend(res["listings"])
+
+        next_page = res["nextPage"]
         if next_page is None:
             return _listings
-
-        query["page"] = next_page
-        return self.search(query, max_depth, _listings)
+        if max_depth > 0 and query.get("page", 1) == max_depth:
+            return _listings
+        return self.search(
+            location=location,
+            available_from=available_from,
+            min_age=min_age,
+            max_age=max_age,
+            lgtb=lgtb,
+            no_kids=no_kids,
+            no_pets=no_pets,
+            non_smoker=non_smoker,
+            min_price=min_price,
+            max_price=max_price,
+            room_type=room_type,
+            max_depth=max_depth,
+            page=next_page,
+            _listings=_listings,
+        )
 
     def get_listing_metadata(self, listing_ids):
         res = self.client.session.post(
